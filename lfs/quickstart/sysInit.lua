8bb373ce2cd40b39d4b0896dca697ba8c77c84f3
mk-pmb <mk-pmb@users.noreply.github.com>
2020-09-07 19:37:21 +0200

    LFS module error message: Fix indentation

--- lfs/quickstart/sysInit.lua
+++ lfs/quickstart/sysInit.lua
@@ -3,7 +3,7 @@
 for pin = 1, 8 do gpio.mode(pin, gpio.OPENDRAIN); gpio.write(pin, 1); end
 
 table.insert(package.loaders, function (m)
-  return node.LFS.get(m) or ('\n\t no module %q in LFS'):format(m)
+  return node.LFS.get(m) or ('\n\tno module %q in LFS'):format(m)
 end)
 
 tmr.create():alarm(2e3, tmr.ALARM_SINGLE, function ()
